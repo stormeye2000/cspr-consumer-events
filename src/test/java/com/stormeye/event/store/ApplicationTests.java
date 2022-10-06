184cf18245805a49939691c83d272e0aa9f7eea6
meywood <105049338+meywood@users.noreply.github.com>
2022-10-06 14:09:35 +0100

    Inital commit of cspr-consumer-events project

---
+++ src/test/java/com/stormeye/event/store/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stormeye.event.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
