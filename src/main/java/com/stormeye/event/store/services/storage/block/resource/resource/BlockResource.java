cea2d701e4cf5d375dfa4f8feaf121d1e4133af6
meywood <105049338+meywood@users.noreply.github.com>
2022-10-12 19:59:31 +0100

    issues/1 - Added tests for sorting in REST API

--- src/main/java/com/stormeye/event/store/services/storage/block/resource/resource/BlockResource.java
+++ src/main/java/com/stormeye/event/store/services/storage/block/resource/resource/BlockResource.java
@@ -4,8 +4,12 @@ import com.stormeye.event.store.services.storage.block.domain.Block;
 import com.stormeye.event.store.services.storage.block.repository.BlockRepository;
 import com.stormeye.event.store.services.storage.common.PageResponse;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -34,7 +38,15 @@ import org.springframework.web.bind.annotation.RestController;
 )
 public class BlockResource {
 
+    private enum SortableFields {
+        blockHeight,
+        eraId,
+        timestamp
+    }
+
+    public static final String TIMESTAMP = "timestamp";
     private final BlockRepository blockRepository;
+
 
     @Autowired
     public BlockResource(final BlockRepository blockRepository) {
@@ -49,23 +61,30 @@ public class BlockResource {
      * @return a page of blocks as JSON
      */
     @GetMapping(value = "/blocks", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(tags = "blocks", summary = "Obtains the blocks",
+            description = "Obtains a page of block that are sortable by timestamp, blockHeight and eraId")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "correctly serving a page of blocks",
+                    content = {@Content(mediaType = "application/json")}),
+    })
     ResponseEntity<PageResponse<Block>> getBlocks(@RequestParam(value = "page", defaultValue = "1", required = false) final int page,
                                                   @RequestParam(value = "size", defaultValue = "10", required = false) final int size,
-                                                  @RequestParam(value = "order_by", defaultValue = "timestamp", required = false) final String orderBy,
+                                                  @RequestParam(value = "order_by", defaultValue = TIMESTAMP, required = false) final SortableFields orderBy,
                                                   @RequestParam(value = "order_direction", defaultValue = "DESC", required = false) final Sort.Direction orderDirection) {
 
 
-        var request = PageRequest.of(page - 1, size, getSort(orderBy, orderDirection));
+        var request = PageRequest.of(page - 1, size, getSort(orderBy.name(), orderDirection));
         return ResponseEntity.ok(new PageResponse<>(blockRepository.findAll(request)));
     }
 
     @NotNull
-    private static Sort getSort(String orderBy, Sort.Direction orderDirection) {
-        if ("timestamp".equals(orderBy)) {
+    static Sort getSort(String orderBy, Sort.Direction orderDirection) {
+        if (TIMESTAMP.equals(orderBy)) {
             return Sort.by(orderDirection, orderBy);
         } else return Sort.by(
                 new Sort.Order(orderDirection, orderBy),
-                new Sort.Order(Sort.Direction.ASC, "timestamp")
+                new Sort.Order(Sort.Direction.ASC, TIMESTAMP)
         );
     }
 
